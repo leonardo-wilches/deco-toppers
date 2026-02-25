ce97707db93cd0de41a3ad27dfd13571b5eb6a8a
yourusernameleonardo-wilches <leo.e.wm@gmail.com>
2026-02-25 01:26:29 -0300

    menu hamburguesa modificado

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -4,4 +4,7 @@ const navBar = document.querySelector('#nav-bar');
 navButton.addEventListener('click', () => {
     navButton.classList.toggle('show');
     navBar.classList.toggle('show');
+
+    const isOpen = navBar.classList.contains('show');
+    navButton.setAttribute('aria-expanded', isOpen);
 });
